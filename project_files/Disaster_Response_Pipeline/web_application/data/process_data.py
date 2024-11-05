0928058243b87c35eee2cdd910493f15f3c1a3bd
Linh Hoang <linhht.419@gmail.com>
2024-11-05 23:52:24 +0900

    add a working version for the Disaster Response Pipeline project

--- project_files/Disaster_Response_Pipeline/web_application/data/process_data.py
+++ project_files/Disaster_Response_Pipeline/web_application/data/process_data.py
@@ -1,16 +1,39 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+def load_data(messages_filepath, categories_filepath) -> pd.DataFrame:
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, how='inner')
+
+    return df
 
 
-def clean_data(df):
-    pass
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    # Split the 'categories' column into multiple seperate category columns
+    categories = df['categories'].str.split(";").apply(pd.Series)
+    row = categories.iloc[0, :]
+    category_colnames = row.apply(lambda x: x[:-2]).to_list()
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+
+    df.drop(columns=['categories'], inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename='DisasterPipelineDB.db'):
+    '''Export the clean dataset into an sqlite database.'''
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('DisasterMessage', engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,12 +47,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- project_files/Disaster_Response_Pipeline/web_application/models/train_classifier.py
+++ project_files/Disaster_Response_Pipeline/web_application/models/train_classifier.py
@@ -1,24 +1,68 @@
 import sys
+import pandas as pd
+import re
+import pickle
+from sqlalchemy import create_engine
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 
-def load_data(database_filepath):
-    pass
+def load_data(database_filepath: str):
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('DisasterMessage', con=engine)
+    X = df['message']
+    Y = df[df.columns.to_list()[4:]]
+    col_names = df.columns[4:].to_list()
+
+    return X, Y, col_names
 
 
-def tokenize(text):
-    pass
+def tokenize(text: str) -> list[str]:
+    stop_words = stopwords.words("english")
+
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize text
+    tokens = word_tokenize(text.lower())
+
+    # lemmatize and remove stop words
+    tokens = [WordNetLemmatizer().lemmatize(w) for w in tokens if w not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('countv', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=10)))
+    ])
+
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_predict = model.predict(X_test)
+    for i, col in enumerate(category_names):
+        print('category:', col)
+        print(classification_report(Y_test[col].to_numpy(), Y_predict[:, i]))
 
 
-def save_model(model, model_filepath):
-    pass
+def save_model(model, model_filepath: str):
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
@@ -27,13 +71,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
-        print('Training model...')
+
+        print('Training model... It may take 15 mins...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
